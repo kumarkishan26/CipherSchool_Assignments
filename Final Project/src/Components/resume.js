4d54d0dba038e940b3061fbfde9b99d8ba0e59b4
kumarkishan26 <43247505+kumarkishan26@users.noreply.github.com>
2020-08-30 23:07:37 +0530

    Add files via upload

---
+++ Final Project/src/Components/resume.js
@@ -0,0 +1,162 @@
+import React, {
+    Component
+} from 'react';
+
+import './styles.css';
+class Resume extends Component {
+    render() {
+        return ( <
+            section className = "resume" >
+
+            <
+            h1 > Resume < /h1>
+
+
+            <
+            div class = "entry" >
+            <
+            h2 > Qualification < /h2> <
+            div class = "content" >
+            <
+            h3 > Year: 2017 - 2021 < /h3> <
+            p > Btech - Information Technology < br / >
+            <
+            em > Lovely Professional University < /em></p >
+            <
+            /div> <
+            div class = "content" >
+            <
+            h3 > Year: 2016 - 2017 < /h3> <
+            p > XII(BSEB) < br / >
+            <
+            em > V S J College, Rajnagar, Madhubani < /em></p >
+            <
+            /div> <
+            div class = "content" >
+            <
+            h3 > Year: 2013 - 2014 < /h3> <
+            p > X(CBSE) < br / >
+            <
+            em > Regional Secondary School, Madhubani < /em></p >
+            <
+            /div> <
+            /div>
+
+            <
+            div class = "entry" >
+            <
+            h2 > Projects < /h2> <
+            div class = "content space" >
+            <
+            h3 > May 2019 - June 2019 < /h3> <
+            p > Pizza Hut Management System < br / >
+            <
+            em > Lovely Professional University, Punjab < /em></p >
+            <
+            ul class = "folio" >
+            <
+            li > Developed a Travel website using Frontend Technology < /li> <
+            br / >
+            <
+            li > Used HTML,CSS,JavaScript,PHP,Laravel < /li> <
+            /ul> <
+            /div> <
+            div class = "content" >
+            <
+            h3 > Oct 2018 - Nov 2018 < /h3> <
+            p > Fitness Calculator < br / >
+            <
+            em > Lovely Professional University < /em></p >
+            <
+            ul class = "folio" >
+            <
+            li > Front End Project using React < /li> <
+            br / >
+            <
+            li > BookShelf Project < /li> <
+            /ul> <
+            /div> <
+            /div>
+
+            <
+            div class = "entry" >
+            <
+            h2 > Certifications < /h2> <
+            div class = "content" >
+            <
+            ul class = "folio" >
+            <
+            li > Certificate
+            for participation in CodeKaze < /li> <
+            br / >
+            <
+            li > Qualified National Engineering Olympiod Round -1</li> <
+            br / >
+            <
+            li > Master C++ Course By Coding Blocks <
+            /li> <
+            br / >
+            <
+            li >
+            Certificate of Advanced Web Concepts by Udemy <
+            /li> <
+            /ul> <
+            /div>
+
+            <
+            /div>
+
+            <
+            div class = "entry" >
+            <
+            h2 > SKILLS < /h2> <
+            div class = "content" >
+            <
+            h3 > Software Development < /h3> <
+            ul class = "skills" >
+            <
+            li > C++,Java,Python < /li> <
+            li > Salesforce < /li> <
+            li > Web Development < /li> <
+            /ul> <
+            /div> <
+            div class = "content" >
+            <
+            h3 > Languages < /h3> <
+            ul class = "skills" >
+            <
+            li > CSS / XHTML < /li> <
+            li > JavaScript / Jquery < /li> <
+            li > React < /li> <
+            li > Saas < /li> <
+            /ul> <
+            /div> <
+            /div>
+
+            <
+            div class = "entry" >
+            <
+            h2 > Personal Details < /h2>
+
+            <
+            p > Date Of Birth: 26th Jan 1999 < /p> <
+            p > Nationality: Indian < /p> <
+            p > Address: Nehru Chowk,Rajnagar Distt: Madhubani (Bihar) < /p>
+
+            <
+            /div>
+
+
+            
+
+
+
+
+
+            <
+            /section>
+        );
+    }
+}
+
+export default Resume;
